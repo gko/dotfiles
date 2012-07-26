194646bf6da532828a8bcb3f9923d6b3be44172c
Konstantin Gorodinskiy <gor.konstantin@gmail.com>
2012-07-26 19:21:31 +0400

    clear screen

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -3,10 +3,10 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ~="cd ~" # `cd` is probably faster to type though
 alias -- -="cd -"
+alias clr="clear"
 
 source $BASH_FOLDER/aliases/osx.bash
 source $BASH_FOLDER/aliases/vim.bash
 source $BASH_FOLDER/aliases/git.bash
 
 alias reload_bash='source ~/.bash_profile'
-
