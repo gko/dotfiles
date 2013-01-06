83769fe3ad9b52d6b832ce5b969ee578e1e367c7
gk <gk@gk-AMILO-Xi-2528.(none)>
2013-01-06 11:23:11 +0400

    red instead of Magenta

--- .bash/themes/colors.bash
+++ .bash/themes/colors.bash
@@ -7,16 +7,14 @@ fi
 if tput setaf 1 &> /dev/null; then
 	tput sgr0
 	if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
-		MAGENTA=$(tput setaf 9)
 		BLUE=$(tput setaf 75)
-		RED=$(tput setaf 1)
+		RED=$(tput setaf 9)
 		ORANGE=$(tput setaf 214)
 		GREEN=$(tput setaf 82)
 		PURPLE=$(tput setaf 141)
 		WHITE=$(tput setaf 255)
 		YELLOW=$(tput setaf 11)
 	else
-		MAGENTA=$(tput setaf 5)
 		RED=$(tput setaf 1)
 		ORANGE=$(tput setaf 1)
 		GREEN=$(tput setaf 2)
@@ -28,7 +26,6 @@ if tput setaf 1 &> /dev/null; then
 	BOLD=$(tput bold)
 	RESET=$(tput sgr0)
 else
-	MAGENTA="\033[1;35m"
 	RED="\033[1;31m"
 	ORANGE="\033[1;33m"
 	GREEN="\033[1;32m"
@@ -40,7 +37,6 @@ else
 	RESET="\033[m"
 fi
 
-export MAGENTA
 export RED
 export YELLOW
 export ORANGE
--- .bash/themes/mine/mine.theme.bash
+++ .bash/themes/mine/mine.theme.bash
@@ -1,4 +1,4 @@
 # For unstaged(*) and staged(+) values next to branch name in __git_ps1
 GIT_PS1_SHOWDIRTYSTATE="enabled"
 
-PS1="\n\[${BOLD}${MAGENTA}\]\u \[$WHITE\]at \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\W\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[$PURPLE\]\$(__git_ps1)\[$WHITE\]\n\$ \[$RESET\]"
+PS1="\n\[${BOLD}${RED}\]\u \[$WHITE\]at \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\W\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[$PURPLE\]\$(__git_ps1)\[$WHITE\]\n\$ \[$RESET\]"
