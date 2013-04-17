780c026683d6f033f3c10921a5c4584ab68ce7c6
gk <gor.konstantin@gmail.com>
2013-04-17 15:21:09 +0400

    +

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cd
 rm -rf $HOME/.bash*&&
 rm -rf ./dotfiles&&
 git clone --depth 1 --recursive https://github.com/gorodinskiy/dotfiles.git&&
-mv dotfiles/.bash_profile $HOME/
-mv dotfiles/.bashrc $HOME/
+mv dotfiles/.bash_profile $HOME/&&
+mv dotfiles/.bashrc $HOME/&&
 rm -rf ./dotfiles
