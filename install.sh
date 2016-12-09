63bd38402c643ff087951654b3bd89525dea14fe
Konstantin Gorodinskiy <mail@konstantin.io>
2016-12-09 22:19:46 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ rm -rf "$HOME"/.bash*&&
 rm -rf ./dotfiles&&
 git clone --depth 1 --recursive https://github.com/gko/dotfiles.git &&
 mv dotfiles/.bash* "$HOME"/ &&
-mv dotfiles/.git* "$HOME"/ &&
+mv dotfiles/.gitc* "$HOME"/ &&
+mv dotfiles/.giti* "$HOME"/ &&
 rm -rf ./dotfiles
