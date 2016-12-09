393cd238700db06dbec7937be15ee61f73c71f67
Konstantin Gorodinskii <konstantin.gorodinskii@docapost.fr>
2016-12-09 21:51:04 +0100

    fix install

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ rm -rf "$HOME"/.bash*&&
 rm -rf ./dotfiles&&
 git clone --depth 1 --recursive https://github.com/gorodinskiy/dotfiles.git &&
 mv dotfiles/.bash* "$HOME"/ &&
+mv dotfiles/.gitignore "$HOME"/ &&
 rm -rf ./dotfiles
