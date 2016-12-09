a1e063ee28dbe70a81e21a0e7549f462ef5e0ec7
Konstantin Gorodinskiy <mail@konstantin.io>
2016-12-09 22:09:33 +0100

    git configs

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ rm -rf "$HOME"/.bash*&&
 rm -rf ./dotfiles&&
 git clone --depth 1 --recursive https://github.com/gko/dotfiles.git &&
 mv dotfiles/.bash* "$HOME"/ &&
-mv dotfiles/.gitignore "$HOME"/ &&
+mv dotfiles/.git* "$HOME"/ &&
 rm -rf ./dotfiles
