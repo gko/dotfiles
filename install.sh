0a43002051fd7841d8235bce7fa55b7f2254cb87
Konstantin Gorodinskii <gor.konstantin@gmail.com>
2016-12-09 22:22:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd ~
 rm -rf "$HOME"/.bash*&&
 rm -rf ./dotfiles&&
-git clone --depth 1 --recursive https://github.com/gko/dotfiles.git &&
+git clone --depth 1 --recursive https://github.com/gko/dotfiles.git ~/dotfiles &&
 mv dotfiles/.bash* "$HOME"/ &&
 mv dotfiles/.gitc* "$HOME"/ &&
 mv dotfiles/.giti* "$HOME"/ &&
