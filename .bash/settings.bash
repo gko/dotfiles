3dbed41eb0b84e643e878be190bbea9b805d09f5
Konstantin Gorodinskiy <mail@konstantin.io>
2016-12-10 11:26:31 +0100

    init keychain only if its there

--- .bash/settings.bash
+++ .bash/settings.bash
@@ -1,14 +1,16 @@
 case $OSTYPE in
   linux*)
-    #bug see https://bugs.launchpad.net/ubuntu/+source/gnome-keyring/+bug/201786
-    unset SSH_AUTH_SOCK
-    unset use_color safe_term match_lhs
+    if [ -f /usr/bin/keychain ]; then
+      #bug see https://bugs.launchpad.net/ubuntu/+source/gnome-keyring/+bug/201786
+      unset SSH_AUTH_SOCK
+      unset use_color safe_term match_lhs
 
-    ### START-Keychain ###
-    # Let  re-use ssh-agent and/or gpg-agent between logins
-    /usr/bin/keychain $HOME/.ssh/id_rsa
-    source $HOME/.keychain/$HOSTNAME-sh
-    ### End-Keychain ###
+      ### START-Keychain ###
+      # Let  re-use ssh-agent and/or gpg-agent between logins
+      /usr/bin/keychain $HOME/.ssh/id_rsa
+      source $HOME/.keychain/$HOSTNAME-sh
+      ### End-Keychain ###
+    fi
     ;;
 esac
 
