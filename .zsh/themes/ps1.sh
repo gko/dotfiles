acdb2c46672ab5e9ff43c6a63a6f924020451c5e
Konstantin Gorodinskii <gor.konstantin@gmail.com>
2017-01-01 22:04:25 +0100

    hostname fix for zsh

--- .zsh/themes/ps1.sh
+++ .zsh/themes/ps1.sh
@@ -1,21 +1,21 @@
 git_super_status() {
-	precmd_update_git_vars
+  precmd_update_git_vars
   if [ -n "$__CURRENT_GIT_STATUS" ]; then
     STATUS="%F{default}%b($GIT_BRANCH)"
 
-	  if [ "$GIT_UNTRACKED" -ne "0" ]; then
-		  STATUS="$STATUS%B%F{red}*%b%F{default}"
-	  fi
-	  if [ "$GIT_CHANGED" -ne "0" ] && [ "$GIT_UNTRACKED" -eq "0" ]; then
-		  STATUS="$STATUS%B%F{red}*%b%F{default}"
-	  fi
-	  echo "$STATUS"
-	fi
+  if [ "$GIT_UNTRACKED" -ne "0" ]; then
+    STATUS="$STATUS%B%F{red}*%b%F{default}"
+  fi
+  if [ "$GIT_CHANGED" -ne "0" ] && [ "$GIT_UNTRACKED" -eq "0" ]; then
+    STATUS="$STATUS%B%F{red}*%b%F{default}"
+  fi
+  echo "$STATUS"
+  fi
 }
 
 # only if we're on remote machine
 if [[ ! $HOSTNAME =~ 'localhost' ]]; then
-  _hostname="%b${default} at \h"
+  _hostname="%b${default} at %m"
 fi
 
 precmd() { 
