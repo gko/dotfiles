c7c32439f4ef6d279bb02d3d1b919c625495a1d8
Konstantin Gorodinskiy <mail@konstantin.io>
2019-03-26 19:48:21 +0100

    settings and stuff

---
+++ settings.sh
@@ -0,0 +1,26 @@
+source ~/.dotfiles/settings/go.sh
+
+case $OSTYPE in
+	linux*)
+		source ~/.dotfiles/settings/keychain.sh
+		;;
+esac
+
+# history
+export SAVEHIST=10000
+export HISTFILE=~/.history
+if [ -n "$ZSH_VERSION" ]; then
+	setopt APPEND_HISTORY
+	setopt INC_APPEND_HISTORY
+fi
+export HISTFILESIZE=10000
+export HISTSIZE=6000
+# export HISTTIMEFORMAT="[%F %T] "
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+
+export EDITOR=/usr/bin/vim
+export TERM=xterm
+export LANG=en_US.UTF-8
+
+# to avoid downloading chromium on every npm install
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
