91164776f7bb12c3492635395e4a86ff7eb77b76
Konstantin Gorodinskii <mail@konstantin.io>
2019-11-18 23:37:27 +0100

    fix tmux ln

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.tmux.conf ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 ln -s ~/.dotfiles/.gitignore ~/.gitignore
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
