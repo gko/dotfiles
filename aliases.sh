327c556c63e5551e87dbb4ea7fedd6b52bafba30
Konstantin Gorodinskii <mail@konstantin.io>
2019-12-08 22:27:03 +0100

    some more aliases

--- aliases.sh
+++ aliases.sh
@@ -23,14 +23,12 @@ alias o="open"
 # overall
 alias video2gif="echo -e \"\n  writing to ~/out.gif\n\" && ffmpeg -vf scale=640:-1 -gifflags +transdiff ~/out.gif -i"
 alias clr="clear"
-alias grep="grep -IE --color=auto"
 alias c="clear"
-alias la="ls -la"
-alias ll="ls -lah"
 alias vi='vim'
 alias v='vim'
 alias nv='nvim'
 alias n='nvim'
+alias e="$EDITOR"
 alias g='git'
 alias prj='project'
 
@@ -51,10 +49,12 @@ alias OPTIONS="curl -i -X OPTIONS -L"
 alias HEAD="curl -I -L"
 
 # https://github.com/LukeSmithxyz/voidrice/blob/efa9fffae21abdcf207678655a446770082afd9a/.config/aliasrc#L17
-ls="ls -hN --color=auto --group-directories-first"
-grep="grep --color=auto"
-diff="diff --color=auto"
-ccat="highlight --out-format=ansi"
+alias ls="ls -hN --color=auto --group-directories-first"
+alias la="ls -la"
+alias ll="ls -lah"
+alias grep="grep --color=auto"
+alias diff="diff --color=auto"
+alias ccat="highlight --out-format=ansi"
 
 # who listens on ports
 alias servers="netstat -tulpn"
