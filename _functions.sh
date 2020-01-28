41e24da725b8b0a3f9c25dcc248db30c81d4a5b8
Konstantin Gorodinskii <mail@konstantin.io>
2020-01-28 04:06:05 +0100

    safeFind –> safe_find

--- _functions.sh
+++ _functions.sh
@@ -1,4 +1,4 @@
 # https://stackoverflow.com/a/40336333
-function safeFind () {
+function safe_find () {
 	{ find "$@" 3>&2 2>&1 1>&3 | grep -v 'Permission denied' | grep -v 'Operation not permitted' >&3; } 3>&2 2>&1
 }
