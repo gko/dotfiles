c697ab87a56ccddde1a565ecd45143da93f9eb43
Konstantin Gorodinskii <mail@konstantin.io>
2020-06-05 07:46:08 +0200

    add MANPAGER if you have neovim installed

--- settings/editor.sh
+++ settings/editor.sh
@@ -2,6 +2,9 @@ if command -v nvim >/dev/null 2>&1; then
 	export EDITOR=nvim
 	export VISUAL=nvim
 	export GIT_EDITOR=nvim
+	# Use nvim as manpager `:h Man`
+	export MANPAGER='nvim +Man!'
+	export MANWIDTH=999
 elif command -v vim >/dev/null 2>&1; then
 	export EDITOR=vim
 	export VISUAL=vim
