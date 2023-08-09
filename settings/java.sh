8cd6d7e69a28217a353b85531cd34846e8357602
Konstantin Gorodinskii <mail@konstantin.io>
2023-08-09 22:28:02 +0200

    add a quick java fix example for openjdk
    still needs testing with android stuff

--- settings/java.sh
+++ settings/java.sh
@@ -1,5 +1,6 @@
 # https://stackoverflow.com/a/33513163
 # https://stackoverflow.com/a/73040512/676756
+# sudo ln -s "$(brew --prefix openjdk)/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk.jdk
 if [[ -e /usr/libexec/java_home ]]; then
 	export JAVA_HOME=`/usr/libexec/java_home`
 fi
