f3ccb02080df5b3216cf90f6cc30f3aaf0110936
Konstantin Gorodinskii <mail@konstantin.io>
2023-09-26 20:42:56 +0200

    add GH_EDITOR just in case

--- settings/editor.sh
+++ settings/editor.sh
@@ -2,6 +2,8 @@ if [ $commands[nvim] ]; then
 	export EDITOR=nvim
 	export VISUAL=nvim
 	export GIT_EDITOR=nvim
+	# https://cli.github.com/manual/gh_help_environment
+	export GH_EDITOR=nvim
 	# Use nvim as manpager `:h Man`
 	export MANPAGER='nvim +Man!'
 	export MANWIDTH=999
@@ -9,8 +11,10 @@ elif [ $commands[vim] ]; then
 	export EDITOR=vim
 	export VISUAL=vim
 	export GIT_EDITOR=vim
+	export GH_EDITOR=vim
 else
 	export EDITOR=vi
 	export VISUAL=vi
 	export GIT_EDITOR=vi
+	export GH_EDITOR=vi
 fi
